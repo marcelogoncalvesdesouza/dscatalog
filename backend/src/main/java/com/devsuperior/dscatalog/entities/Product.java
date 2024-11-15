fe427ef151cb07a1b4f50ab48666b52cf20b59ef
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2024-11-15 01:42:51 -0300

    Product entity and SQL script

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -4,17 +4,26 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant createdAt; // Salva em UTC
@@ -22,10 +31,16 @@ public class Category implements Serializable {
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt; // Salva em UTC
 
-    public Category() {
+    @ManyToMany//(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    Set<Category> categories = new HashSet<>();
+
+    public Product() {
     }
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name) {
         this.id = id;
         this.name = name;
     }
@@ -44,6 +59,42 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     public Instant getCreatedAt() {
@@ -69,7 +120,7 @@ public class Category implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Category category = (Category) o;
+        Product category = (Product) o;
 
         if (!Objects.equals(id, category.id)) return false;
         return Objects.equals(name, category.name);
